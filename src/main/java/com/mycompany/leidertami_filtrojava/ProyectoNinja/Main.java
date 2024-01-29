88792257d40a45f22ba5e664d7468775f984b27d
leidert11 <ltami216@gmail.com>
2024-01-29 13:54:33 -0500

    feat: :ambulance: corrigiendo un error en el menu

--- src/main/java/com/mycompany/leidertami_filtrojava/ProyectoNinja/Main.java
+++ src/main/java/com/mycompany/leidertami_filtrojava/ProyectoNinja/Main.java
@@ -18,7 +18,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int opcion = 0;
-        while (opcion != 4) {
+        while (opcion != 5) {
             System.out.println("Menu:");
             System.out.println("1. Listar ninjas con habilidades");
             System.out.println("2. mostrar las misiones disponibles para  un ninja en especifico");
